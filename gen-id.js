bd64c15b96084fd2e2aaf57cddfd41a4611e9be0
𝘼𝙧𝙨𝙡𝙖𝙣 𝘾𝙝𝙖𝙪𝙙𝙖𝙧𝙮 <143324285+Arslan-Chaudhary313@users.noreply.github.com>
2026-04-21 16:23:24 +0500

    Refactor makeid function for ID generation

--- gen-id.js
+++ gen-id.js
@@ -1,10 +1,18 @@
+/**
+ * ᴍᴀ𝐫𝐜-𝐦𝐝 ID Generator
+ * Developed by: 𝐀𝐫𝐬𝐥𝐚𝐧 𝐂𝐡𝐚𝐮𝐝𝐡𝐚𝐫𝐲 👑
+ */
+
 function makeid(num = 4) {
   let result = "";
-  let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  var characters9 = characters.length;
-  for (var i = 0; i < num; i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters9));
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+  
+  for (let i = 0; i < num; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
+  
   return result;
 }
-module.exports = {makeid};
+
+module.exports = { makeid };
